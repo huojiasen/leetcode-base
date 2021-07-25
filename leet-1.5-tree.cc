6d984e9fc5eb04c78be4d427dd33ef7dd6e47bbf
huojiasen <jiasen_huo@126.com>
2021-07-25 13:45:37 +0800

    Update leet-1.5-tree.cc

--- leet-1.5-tree.cc
+++ leet-1.5-tree.cc
@@ -1,1 +1,30 @@
 //tree
+
+struct TreeNode {
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> inorderTraversal(TreeNode* root) {
+  vector<int> res;
+  stack<TreeNode*> st;
+  TreeNode* p = root;
+  while(!st.empty()||p) {
+    if (p) {
+      st.push(p);p = p->left;
+    } else {
+      p = st.top();st.pop();
+      res.push_back(p->val);
+      p = p->right;
+    }
+  }     
+  return res;
+}
+void inorder(TreeNode* root, vector<int>& res) {
+  if (root!=NULL) {
+    inorder(root->left, res);
+    res.push_back(root->val);
+    inorder(root->right, res);
+  }
+}
