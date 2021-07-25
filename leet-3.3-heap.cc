b1d7ba1bdfcf3ed48b63e6e1d8facb499d9e238f
huojiasen <jiasen_huo@126.com>
2021-07-25 11:33:41 +0800

    Update leet-3.3-heap.cc

--- leet-3.3-heap.cc
+++ leet-3.3-heap.cc
@@ -1,1 +1,20 @@
 //heap
+
+#include <queue>
+void test() {
+  priority_queue<int> pque;
+  pque.push(10);
+  while(!pque.empty()) {
+    int t = pque.top();
+    pque.pop();
+  }
+}
+
+int heap[1000], sz = 0;
+void push(int x) {
+
+}
+
+int pop() {
+
+}
