ca68478f4aeb01dd3d5725ea846eb79ea8b42736
huojiasen <jiasen_huo@126.com>
2021-07-25 09:41:28 +0800

    Update leet-1.3-queue.cc

--- leet-1.3-queue.cc
+++ leet-1.3-queue.cc
@@ -1,1 +1,27 @@
 //queue
+#include <queue>
+void test() {
+  queue<int> que;
+  que.push(1);
+  que.front();
+  que.pop();
+}
+
+template<typename T> class queue {
+public:
+  queue(int size):max(size) {
+    data = new T[max];
+    front=rear=0;
+  }
+  void push(T e) {
+    if (front==(rear+1)%max) return;
+    data[(++rear)%max] = x;
+  }
+  T pop() {
+    if (front == rear) return;
+    return data[(front++)%max];
+  }
+private:
+  T* data;
+  int max, front, rear;
+};
