b06e9be12762b6087e0a86d868bc04ef7a90029b
huojiasen <jiasen_huo@126.com>
2021-07-25 09:36:20 +0800

    Update leet-1.2-stack.cc

--- leet-1.2-stack.cc
+++ leet-1.2-stack.cc
@@ -1,1 +1,28 @@
 //stack
+#include <stack>
+void test() {
+  stack<int> st;
+  st.push(1);st.top();st.pop();
+}
+// self relization
+template<typename T> class stack {
+public:
+  stack(int size):max(size) {
+    data = new T[max];
+    top = -1;
+  }
+  void push(T e) {
+    if (top==(max-1)) {
+      return;
+    }
+    data[++top] = e;
+  } 
+  T pop() {
+    if (top == -1) return;
+    return data[top--];
+  }
+private:
+  T *data;
+  int max;
+  int top;
+}
