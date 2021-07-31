b1f9dd009fb43254e43718d78e767298feaf7eaf
huojiasen <jiasen_huo@126.com>
2021-07-31 10:42:05 +0800

    Update leet-1.8-sort.cc

--- leet-1.8-sort.cc
+++ leet-1.8-sort.cc
@@ -1,1 +1,21 @@
 //sort
+//1.1 insert sort  1.2shell sort  
+// too easy
+
+//2.1 bubble sort  
+// too easy
+
+2.2 quick sort
+
+
+//3.1 selection sort  
+// too easy
+
+3.2 heap sort
+//
+
+4 merge sort
+
+
+//5 redix sort
+// too easy
