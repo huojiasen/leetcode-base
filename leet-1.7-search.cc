0a866a04d20d57719fd17f25856d1cdc6f1f08e6
huojiasen <jiasen_huo@126.com>
2021-07-31 10:36:07 +0800

    Update leet-1.7-search.cc

--- leet-1.7-search.cc
+++ leet-1.7-search.cc
@@ -1,1 +1,21 @@
 //search
+//1 bst balance tree avl rb-tree
+#include <map>
+void test_bst() {
+  map<int, int> m;
+  m.insert(make_pair<int, int>(1, 10));
+  auto it = m.find(1);
+  if (it!=m.end()) {
+    cout<< it->second;
+  }
+}
+// 2 hash table
+#include <unordered_map>
+void test_hash_table() {
+  unordered_map<int, int> m;
+  m.insert(make_pair<int, int>(1, 10));
+  auto it = m.find(1);
+  if (it!=m.end()) {
+    cout<< it->second;
+  }
+}
