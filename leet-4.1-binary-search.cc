fe4e539f1f97791ad8eb33fb5dd7b11671724cf6
huojiasen <jiasen_huo@126.com>
2021-07-31 10:11:22 +0800

    Create leet-4.1-binary-search.cc

--- leet-4.1-binary-search.cc
+++ leet-4.1-binary-search.cc
@@ -1,1 +1,12 @@
 //binary search
+//1 base binary search 1946-1962
+int binarySearch(int a[], int n, int t) {
+  int i=0, j= n-1;
+  while(i<=j) {
+    int k = i+(j-i)/2;
+    if (t == a[k]) return k;
+    if (t>a[k]) i = k+1;
+    else j = k-1;
+  }
+  return -1;
+}
