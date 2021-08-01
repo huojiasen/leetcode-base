7cb2c83ea4dfb2b13d639b0f20b1dcea6a058d33
huojiasen <jiasen_huo@126.com>
2021-08-01 10:40:34 +0800

    Update leet-2.4-dfs-bfs.cc

--- leet-2.4-dfs-bfs.cc
+++ leet-2.4-dfs-bfs.cc
@@ -1,30 +1,28 @@
-const int n = 100;
-vector<int> G[n]
-bool visited[n]
+//1 permutation
+int x[n]
 
-void dfs(vector<int>* g, int v) {
-  cout<<v<<endl;
-  visited[v] = 1;
-  for (int i=0;i<g[v].size();i++) {
-    if (visited[g[v][i]] == 0) {
-      dfs(g, g[v][i]);
-    }
+void bt(int t) {
+  if (t == n) {
+    print(x);
+    return;
+  }
+  for (int i=t;i<n;i++) {
+    swap(x[i], x[t]);
+    bt(t+1);
+    swap(x[i], x[t]);
   }
 }
 
-void bfs(vector<int>* g, int v) {
-  queue<int> que;
-  cout<<v<<endl;
-  visited[v] = 1;
-  que.push(v);
-  while(!que.empty()) {
-    int t = que.front();que.pop();
-    for (int i=0;i<g[t].size();i++) {
-      if (visited[g[t][i]]==0) {
-        cout<<g[t][i]<<endl;
-        visited[g[t][i]] = 1;
-        que.push(g[t][i]);
-      }
-    }
+//2 subset
+
+int x[n];
+
+void bt(int t) {
+  if (t == n) {
+    print(x);
+    return;
   }
+  x[i] = 0;bt(t+1);
+  x[i] = 1;bt(t+1);
 }
+
