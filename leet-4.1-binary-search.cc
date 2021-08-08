fa21bde3fdef6c45552eff6a996979a30df64075
huojiasen <jiasen_huo@126.com>
2021-08-08 11:37:15 +0800

    Update leet-4.1-binary-search.cc

--- leet-4.1-binary-search.cc
+++ leet-4.1-binary-search.cc
@@ -1,5 +1,5 @@
 //binary search
-//1 base binary search 1946-1962
+//1 base binary search 1946-1962， 一个正确的二分法 人类用了16年，当然没有高级语言
 int binarySearch(int a[], int n, int t) {
   int i=0, j= n-1;
   while(i<=j) {
@@ -9,4 +9,22 @@ int binarySearch(int a[], int n, int t) {
     else j = k-1;
   }
   return -1;
+}
+// 2 二分法扩展
+int solve(vector<int> L,int n, int r) {//n段绳子， 每段长Li， 切割成r个相等的段，可以达到的最大长度
+  double lb = 0, ub = max(L)+1;
+  for (int i = 0;i< 100;i++) {
+    double mid = (lb+ub)/2;
+    if (C(mid)) lb = mid;
+    else ub = mid;
+  }
+  return (int)ub;
+}
+
+bool C(double x, vector<int> L, int n, int r) {// 判定函数，每段按照x来切分，能切割为多少段
+  int num = 0;
+  for (int i=0;i<n;i++) {
+    num += (int)(L[i]/x);
+  }
+  return num >= r;
 }
