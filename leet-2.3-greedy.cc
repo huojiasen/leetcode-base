7e53be668d58f88863e69ed9cc05663a856c3b77
huojiasen <jiasen_huo@126.com>
2021-08-08 09:05:31 +0800

    Update leet-2.3-greedy.cc

--- leet-2.3-greedy.cc
+++ leet-2.3-greedy.cc
@@ -1,1 +1,15 @@
-//greedy
+//greedy easy but need more practise
+int maxArea(vector<int> &height) {
+  int start = 0, end = height.size() - 1;
+  int result = INT_MIN;
+  while (start < end) {
+    int area = min(height[end], height[start]) * (end - start);
+    result = max(result, area);
+    if (height[start] <= height[end]) {
+      start++;
+    } else {
+      end--; 
+    }
+  }
+  return result;
+}
