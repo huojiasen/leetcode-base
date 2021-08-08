b4045edcb089a980e2af6fa721afb2e7a65796cd
huojiasen <jiasen_huo@126.com>
2021-08-08 11:55:22 +0800

    Update leet-3.4-seg-tree.cc

--- leet-3.4-seg-tree.cc
+++ leet-3.4-seg-tree.cc
@@ -1,1 +1,20 @@
 //seg-tree
+const int n = 1000;
+int A[n];
+struct node {
+  int left, right;
+  int sum;
+};
+node seg-tree[n*2];
+
+void build(int t, int l, int r) {
+
+}
+
+int query(int t, int l, int r) {
+
+}
+
+void update(int t, int l ,int r, int pos, int del) {
+
+}
