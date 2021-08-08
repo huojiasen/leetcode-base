4b5c5774bbcfa5331f11d46a721f5e41172be897
huojiasen <jiasen_huo@126.com>
2021-08-08 11:42:34 +0800

    Update leet-4.2-sliding-window.cc

--- leet-4.2-sliding-window.cc
+++ leet-4.2-sliding-window.cc
@@ -1,1 +1,11 @@
-
+// 滑动窗口，最短的连续字段和大于S， O(n*n)--> O(n)
+void solve(vector<int> a, int S) {
+  int res = a.size()+1, s = 0, e = 0, sum = 0;
+  while(true) {
+    while(e<n && sum<S) sum += a[e++];
+    if(sum<S) break;
+    res = min(res, e-s);
+    sum -= a[s++];
+  }
+  return res;
+}
