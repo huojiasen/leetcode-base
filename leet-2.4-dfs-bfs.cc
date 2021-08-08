e2b74dd5f6521a7eb2f602271816f9d3f117e9bd
huojiasen <jiasen_huo@126.com>
2021-08-08 09:30:29 +0800

    Update leet-2.4-dfs-bfs.cc

--- leet-2.4-dfs-bfs.cc
+++ leet-2.4-dfs-bfs.cc
@@ -25,4 +25,45 @@ void bt(int t) {
   x[i] = 0;bt(t+1);
   x[i] = 1;bt(t+1);
 }
+//3 connected part in a picture
+void dfs(int x, int y, vector<vecotr<int>>& pic) {
+  pic[x][y] = -1;
+  for (int i=-1;i<=1;i++) {
+    for (int j=-1;j<=1;j++) {
+      int nx = x + i, ny = y + j;
+      if (nx>=0&&nx<=pic.size()&&ny>=0&&ny<=pic[nx].size()&&pic[nx][ny]>0) dfs(nx, ny, pic);
+    }
+  }
+  return;
+}
+
+
+void bfs(int x, int y, vector<vector<int>>& pic) {
+  queue<pair<int, int>> que;
+  pic[x][y] = -1;que.push(make_pair(x, y));
+  while(!que.empty()) {
+    pair<int, int> t = que.front();que.pop();
+    for (int i=-1;i<=1;i++) {
+      for (int j=-1;j<=1;j++) {
+        int nx = t->first + i, ny = t->second + j;
+        if (nx>=0&&nx<=pic.size()&&ny>=0&&ny<=pic[nx].size()&&pic[nx][ny]>0) {
+          que.push(make_pair(nx, ny));pic[nx][ny]=-1;
+        }
+      }
+    }
+  }
+}
+int solve(vector<vector<int>> pic) {
+  int res = 0;
+  for (int i=0;i<pic.size();i++) {
+    for (int j=0;j<pic[i].size();j++) {
+      if (pic[i][j]>0) {
+        //dfs(i, j, pic);
+        bfs(i, j, pic);
+        res ++;
+      }
+    }
+  }
+  return res;
+}
 
